37cd68f96df7b5d0b94004ffa93607337c0ee728
alyssadm <ahlisa.dm@gmail.com>
2019-08-02 21:24:45 -0400

    added a prompt body option

--- models/prompts.js
+++ models/prompts.js
@@ -7,12 +7,13 @@ const mongoose = require('mongoose');
 
 // SCHEMA
 const promptSchema = mongoose.Schema({
-  prompt: {type: String, required: true},
+  title: {type: String, required: true},
+  body: String,
   author: {type: String, required: true},
   tags: [String],
   responses: [{author: String, body: String, date: Date}],
   likes: {type: Number, default: 0, min: 0}
-})
+});
 
 // MODEL
 const Prompt = mongoose.model('Prompts', promptSchema);
--- models/users.js
+++ models/users.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 // I tried just requiring prompts.js but Mongoose won't let me use a model as a Schema type so I had to paste it here again
 const promptSchema = mongoose.Schema({
-  prompt: {type: String, required: true},
+  title: {type: String, required: true},
+  body: String,
   author: {type: String, required: true},
   tags: [String],
   responses: [{author: String, body: String, date: Date}],
