76eff1adc37e6751b1f85d7fd5cd1d801ff83800
alyssadm <ahlisa.dm@gmail.com>
2019-08-06 22:08:57 -0400

    styled the API quote. prevented users from not entering a password.

--- controllers/signup.js
+++ controllers/signup.js
@@ -16,20 +16,31 @@ signup.get('/', (req, res) => {
 
 // POST
 signup.post('/', (req, res) => {
-    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-    if (req.body.avatar === ""){
-      req.body.avatar = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/500px-User_font_awesome.svg.png"
+
+    // Accounts for the user not putting anything into the fields
+    if (req.body.password === "" || req.body.username === ""){
+      res.send("<p>Password and username are required. <a href='/signup'>Return</a>")
+    } else {
+
+      if (req.body.avatar === ""){
+        req.body.avatar = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/500px-User_font_awesome.svg.png"
+      }
+
+      // Hashes the password
+      req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+
+      // Creates the user. If the username is already taken, brings them back to the signup page.
+      Models.User.create(req.body, (err, user) => {
+        if (err && err.code === 11000){
+          res.render('signup/new.ejs', {tabTitle: "Sign Up", currentUser: req.session.currentUser, error: true})
+        } else if (err && err.code != 11000){
+          res.send('Something went wrong. Contact an administrator or <a href="/">return here.</a>')
+        } else {
+          req.session.currentUser = user;
+          res.redirect('/')}
+      })
     }
 
-    Models.User.create(req.body, (err, user) => {
-      if (err && err.code === 11000){
-        res.render('signup/new.ejs', {tabTitle: "Sign Up", currentUser: req.session.currentUser, error: true})
-      } else if (err && err.code != 11000){
-        res.send('Something went wrong. Contact an administrator or <a href="/">return here.</a>')
-      } else {
-        req.session.currentUser = user;
-        res.redirect('/')}
-    })
 });
 
 module.exports = signup;
